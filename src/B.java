8cd2db094d17157c3ff9e474e94fbb36b2108413
Sajith Mohan Plavalappil <sajithmohan1991992@gmail.com>
2024-02-05 13:44:46 -0500

    base display

--- src/B.java
+++ src/B.java
@@ -1,6 +1,11 @@
 class B extends A  {
-    void show(){
+    void display(){
         System.out.println("this is class B");
+    }
+    void basedisplay(){
+
+        super.display();
+
     }
     
 }
