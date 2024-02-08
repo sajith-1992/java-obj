658930faf4aeb98694d07a1f1f3a3ed7ee199994
Sajith Mohan Plavalappil <sajithmohan1991992@gmail.com>
2024-02-08 07:25:13 -0500

    thread different object

--- package.jav/thread/src/App.java
+++ package.jav/thread/src/App.java
@@ -3,5 +3,11 @@ public class App  {
         
         SampleThread st= new SampleThread();
         st.start();
+        SampleThread st1= new SampleThread();
+        st1.start();
+        SampleThread st2= new SampleThread();
+        st2.start();
+
+
         }
 }
